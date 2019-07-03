e262fc8d01631c69b0f95866bf2aebe0445fb4f9
Cyril <cyril.crozes@gmail.com>
2019-07-03 07:45:59 +0000

    Mis en place des horaires + sql

---
+++ horaires/horaire.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>DECLARARTION HEURES MENSUELLES</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid bg-primary text-white">
+		<div class="container">
+			<h1 class="display-3">Déclaration des heures</h1>
+			<p class="lead">Outil de déclaration des heures faites</p>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+            
+		</div>
+	</div>
+</body>
+</html>
