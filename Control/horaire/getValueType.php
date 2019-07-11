021d5df90526cc98f3afd5d9530954fe2e1c21be
Crozes Cyril <crozes-cyril@hotmail.fr>
2019-07-11 22:57:08 +0200

    encule d'encodage de merde

--- Control/horaire/getValueType.php
+++ Control/horaire/getValueType.php
@@ -4,7 +4,7 @@
 
     include '../all/log_db.php';
     try{
-        $PDO = new PDO('mysql:host='.$DB_serveur.';dbname='.$DB_base.'',$DB_utilisateur,$DB_motdepasse);
+        $PDO = new PDO('mysql:host='.$DB_serveur.';dbname='.$DB_base.';charset=utf8',$DB_utilisateur,$DB_motdepasse);
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
         //$PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); 
         $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
