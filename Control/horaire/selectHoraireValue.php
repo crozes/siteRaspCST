7962c13e6851cb730b9a738e9d567a7fcf43aa52
Crozes Cyril <crozes-cyril@hotmail.fr>
2019-07-11 22:55:45 +0200

    debug2

--- Control/horaire/selectHoraireValue.php
+++ Control/horaire/selectHoraireValue.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    header('Content-Type: text/html; charset=utf-8');
-    //header('Content-type: application/json');
+    //header('Content-Type: text/html; charset=utf-8');
+    header('Content-type: application/json');
 
     if(isset($_GET)){
         $month = $_GET["month"];
@@ -10,7 +10,7 @@
 
     include '../all/log_db.php';
     try{
-        $PDO = new PDO('mysql:host='.$DB_serveur.';dbname='.$DB_base.'',$DB_utilisateur,$DB_motdepasse);
+        $PDO = new PDO('mysql:host='.$DB_serveur.';dbname='.$DB_base.';charset=utf8',$DB_utilisateur,$DB_motdepasse);
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
         //$PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); 
         $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -20,23 +20,13 @@
 
     $sql="  SELECT h.idHoraire, h.dateHoraire, h.timeHoraire, h.comHoraire, li.nomLieuInter, ti.nomTypeInter FROM Horaire h INNER JOIN LieuInter li ON h.idLieuInter = li.idLieuInter INNER JOIN TypeInter ti ON h.idTypeInter = ti.idTypeInter WHERE `dateHoraire` >= '".$year."-".$month."-00 00:00:00' AND `dateHoraire` <='".$year."-".$month."-31 23:59:59' AND `idPersonne` = ".$_SESSION['Auth'][0]->idPersonne." ORDER BY dateHoraire;";
 
-    error_log ($sql);
-
     $req = $PDO->prepare($sql);
     $req->execute();
     $data = $req->fetchAll();
-
-    //error_log (print_r($data));
 
     $json  = json_encode($data);
 
     error_log (json_last_error_msg());
 
     echo $json;
-
-    /*foreach($data as $val){
-        //print_r($val);
-        //echo $val['dateHoraire'];
-    }*/
-
 ?>
