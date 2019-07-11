8f1524c0ddd1cf5c0276271f67f01a4d331cae6e
Crozes Cyril <crozes-cyril@hotmail.fr>
2019-07-11 23:05:20 +0200

    menu

--- View/menu/menu_item.php
+++ View/menu/menu_item.php
@@ -1,14 +1,12 @@
-<ul class="nav nav-tabs" id="navId">
-    <li class="nav-item">
-        <a id="navbarSupportedContent" class="nav-link" href="?page=licence">Licence Auto</a>
-    </li>
-    <li class="nav-item">
-        <a id="navbarSupportedContent" class="nav-link" href="?page=horaire">Déclaration Horaire</a>
-    </li>
-</ul>
-
-
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav nav-tabs" id="navId">
+        <li class="nav-item">
+            <a id="navbarSupportedContent" class="nav-link" href="?page=licence">Licence Auto</a>
+        </li>
+        <li class="nav-item">
+            <a id="navbarSupportedContent" class="nav-link" href="?page=horaire">Déclaration Horaire</a>
+        </li>
+    </ul>
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a id="navbarSupportedContent" class="nav-link" href="#"><?php echo $_SESSION['Auth'][0]->prenomPersonne." ".$_SESSION['Auth'][0]->nomPersonne; ?></a>
