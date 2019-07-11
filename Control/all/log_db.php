dbc83efdbc0c531bc36ba71f4b49da195d5159c4
Cyril <cyril.crozes@gmail.com>
2019-07-11 19:34:51 +0000

    restor log

---
+++ Control/all/log_db.php
@@ -0,0 +1,8 @@
+<?php
+    //$DB_serveur = 'localhost:8889'; // Nom du serveur
+    $DB_serveur = 'localhost:3306'; // Nom du serveur
+    $DB_utilisateur = 'root'; // Nom de l'utilisateur de la base
+    //$DB_motdepasse = 'root'; // Mot de passe pour accèder à la base
+    $DB_motdepasse = 'Nanahara-31520'; // Mot de passe pour accèder à la base
+    $DB_base = 'CST'; // Nom de la base
+?>
